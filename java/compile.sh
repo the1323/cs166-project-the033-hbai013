b70372d9c66aa4b60597b8550f886d67d9fdfe20
the1323 <48039916+the1323@users.noreply.github.com>
2021-06-07 14:46:00 -0700

    Update compile.sh

--- java/compile.sh
+++ java/compile.sh
@@ -1,3 +1,3 @@
 #! /bin/bash
 rm -rf bin/*.class
-javac -cp ".;java/lib/postgresql-42.1.4.jar;" java/src/DBproject.java -d bin/
+javac -cp ".;java/lib/postgresql-42.1.4.jar;" java/src/DBproject.java -d java/bin/
