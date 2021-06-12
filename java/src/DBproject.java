846e33e8aca007485a5b3c63a0690f14320d234a
the1323 <48039916+the1323@users.noreply.github.com>
2021-06-11 21:30:03 -0700

    Update DBproject.java

--- java/src/DBproject.java
+++ java/src/DBproject.java
@@ -567,7 +567,10 @@ public class DBproject {
                     System.out.println("status: " + foundstatus);
                     System.out.println("Enter Patient Details \n Enter Patient ID (if you are new patient, enter 'x'): ");
                     patient_id = in.readLine();
-                    while(!checkint(patient_id) || patient_id!="x"){
+                    while(!checkint(patient_id) ){
+                        if (patient_id == "x") {
+                            break;
+                        }
                         System.out.println("Invalid Input, Try Again.");
                         patient_id = in.readLine();
                     }
